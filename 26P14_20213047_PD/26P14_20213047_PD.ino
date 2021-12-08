bbbb22d23e703321ffacf85e644941719adea144
이예원 <81704418+scarletYewon@users.noreply.github.com>
2021-12-08 14:13:10 +0900

    PD수정

--- 26P14_20213047_PD/26P14_20213047_PD.ino
+++ 26P14_20213047_PD/26P14_20213047_PD.ino
@@ -1,14 +1,16 @@
 #include <Servo.h>
 
+// Configurable parameters
+
 // Arduino pin assignment
 #define PIN_LED 9  //LED in 9
 #define PIN_SERVO 10 // SERVO in 10
 #define PIN_IR A0  // Infrared sensor signal in A0
 
 // Framework setting
-#define _DIST_MIN 110
-#define _DIST_TARGET 300  // Target distance = 25.5cm
-#define _DIST_MAX 400
+#define _DIST_MIN 117
+#define _DIST_TARGET 330  // Target distance = 25.5cm
+#define _DIST_MAX 320
 
 // Distance sensor
 #define _DIST_ALPHA 0.35  // EMA filter
@@ -17,15 +19,13 @@
 const float coE[] = {0.0000204, -0.0136853, 4.0482617, -156.8686997};
 
 // Servo range
-#define _DUTY_MIN 1140  // SERVO DUTY's minimum 
-#define _DUTY_NEU 1340  // SERVO DUTY's neutral 
-#define _DUTY_MAX 1540  // SERVO DUTY's maximum 
+#define _DUTY_MIN 1145  // SERVO DUTY's minimum 
+#define _DUTY_NEU 1455  // SERVO DUTY's neutral 
+#define _DUTY_MAX 1545  // SERVO DUTY's maximum 
 
 // Servo speed control
 #define _SERVO_ANGLE 30  // SERVO ANGLE 
-#define _SERVO_SPEED 100  // SERVO SPEED 
-#define _RAMPUP_TIME 360 // servo speed rampup (0 to max) time (unit: ms)
-
+#define _SERVO_SPEED 150  // SERVO SPEED 
 
 // Event periods
 #define _INTERVAL_DIST 15 
@@ -33,9 +33,9 @@ const float coE[] = {0.0000204, -0.0136853, 4.0482617, -156.8686997};
 #define _INTERVAL_SERIAL 100 
 
 // PID parameters
-#define _Kp 1.0 // PID Propotional gain for P
-#define _Ki 0.0075  // PID Propotional gain for I
-#define _Kd 850  // PID Propotional gain for D
+#define _Kp 0.5 // PID Propotional gain for P
+#define _Ki 0.0  // PID Propotional gain for I
+#define _Kd 269.0  // PID Propotional gain for D
 
 // global variables 
 float dist_min, dist_max, dist_raw, dist_ema, alpha;
@@ -51,18 +51,11 @@ unsigned long last_sampling_time_dist, last_sampling_time_servo, last_sampling_t
 bool event_dist, event_servo, event_serial;
 
 // Servo speed control
-int duty_chg_per_interval,duty_chg_max,duty_chg_adjust,toggle_interval, toggle_interval_cnt;
-
-float pause_time; // unit: sec
+int duty_chg_per_interval; 
 int duty_target, duty_curr;
-
-
-#define START _DUTY_MIN + 100
-#define END _DUTY_MAX - 100
 
 // PID variables
 float error_curr, error_prev, control, P_term, I_term, D_term;
-float D_termS;
 
 // Filter
 #define _INTERVAL_DIST 30  // DELAY_MICROS * samples_num^2 의 값이 최종 거리측정 인터벌임. 넉넉하게 30ms 잡음.
@@ -85,32 +78,16 @@ void setup() {
   dist_target = _DIST_TARGET;
   alpha = _DIST_ALPHA;
 
-  duty_target = duty_curr;
+  duty_target = duty_curr = 1350;
 
 // move servo to neutral position
   myservo.writeMicroseconds(_DUTY_NEU);
 
 // initialize serial port
-  Serial.begin(115200);
-
+  Serial.begin(57600);
 
 // convert angle speed into duty change per interval.
   duty_chg_per_interval = (_DUTY_MAX - _DUTY_MIN) * ((float)_SERVO_SPEED / _SERVO_ANGLE) * ((float)_INTERVAL_SERVO / 1000); //[3128]
-
-// servo related variables
-  //duty_chg_max = (float)(_DUTY_MAX - _DUTY_MIN) * _SERVO_SPEED / 180 * _INTERVAL_SERVO / 1000;
-  //duty_chg_adjust = (float) duty_chg_max * _INTERVAL_SERVO / _RAMPUP_TIME;
-  //duty_chg_per_interval = 0; // initial speed is set to 0.
-
-
-// initialize variables for duty_target update.
-  pause_time = 0.5;
-  toggle_interval = (180.0 / _SERVO_SPEED + pause_time) * 1000 / _INTERVAL_SERVO;
-  toggle_interval = 1; // to demonstrate overshoot
-  toggle_interval_cnt = toggle_interval;
-  
-// initialize last sampling time
-  last_sampling_time_servo = 0;
 }
   
 void loop() {  // Event generator
@@ -120,7 +97,6 @@ void loop() {  // Event generator
     event_dist = true;
   }
   if (millis() >= last_sampling_time_servo + _INTERVAL_SERVO) {
-    
     last_sampling_time_servo += _INTERVAL_SERVO;
     event_servo = true;
   }
@@ -139,18 +115,12 @@ void loop() {  // Event generator
   // PID control logic
     error_curr = dist_target - dist_raw;
     P_term = _Kp * error_curr;
-    I_term += _Ki * error_curr;
+    I_term += error_curr * 20;
     D_term = _Kd * (error_curr - error_prev) / 20;
-    control = P_term + D_term + I_term;
+    control = P_term + D_term;
 
     //update error_prev
     error_prev = error_curr;
-
-    //D_term이 너무 커서 Serial 포트에는 800이 넘어가지 않도록
-    //Serial 용 변수를 하나 만들겠습니다.
-    D_termS = D_term;
-    if (D_termS > 800) D_termS = 800;
-
     
   // duty_target = f(duty_neutral, control)
     duty_target = _DUTY_NEU + control;
@@ -179,23 +149,17 @@ void loop() {  // Event generator
   if(event_serial) {
     event_serial = false;
     
-    Serial.print("IR:");
-    Serial.print(dist_raw );
-    Serial.print(" ,T:");
-    Serial.print(dist_target);
-    Serial.print(",P:");
+    Serial.print("dist_ir:");
+    Serial.print(dist_raw);
+    Serial.print(",pterm:");
     Serial.print(map(P_term,-1000,1000,510,610));
-    Serial.print(",D:");
-    Serial.print(map(D_termS,-1000,1000,510,610));
-    Serial.print(",I:");
-    Serial.print(map(I_term,-1000,1000,510,610));
-    Serial.print(",DTT:");
+    Serial.print(",dterm:");
+    Serial.print(map(D_term,-1000,1000,510,610));
+    Serial.print(",duty_target:");
     Serial.print(map(duty_target,1000,2000,410,510));
-    Serial.print(",DTC:");
+    Serial.print(",duty_curr:");
     Serial.print(map(duty_curr,1000,2000,410,510));
-    Serial.println(",-G:245, +G:265, m:0, M:800");
-    
-  
+    Serial.println(",Min:117,Low:140,dist_target:250,High:310,Max:450");
   }
 }
 
